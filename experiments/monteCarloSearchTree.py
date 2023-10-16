f2013a7e00f11a69c6bd10d4c5899ff6927da020
Skyler Atchison <skyler.atchison145@myci.csuci.edu>
2023-10-16 10:01:11 -0700

    Made some more tests

--- experiments/monteCarloSearchTree.py
+++ experiments/monteCarloSearchTree.py
@@ -1,16 +1,33 @@
 '''
 Simple Monte Carlo Search Tree
 Implementation based on GeeksForGeeks: https://www.geeksforgeeks.org/ml-monte-carlo-tree-search-mcts/
+This page was helpful as well: https://towardsdatascience.com/monte-carlo-tree-search-an-introduction-503d8c04e168
 '''
 
 from typing import Self
-import random
+import random, math
+from connectFour import Board
+import connectFour as game
 
 MAX_SEARCH_TIME = 1000000
+COMPUTER_PLAYER = 'X'
+OPPOSING_PLAYER = 'O'
+UCB_CONSTANT = 0.3 # TODO: I don't actually know what the best value for this constant is
 
 class Stats:
-    def __init__(self, visits: int):
-        self.visits = visits
+    def __init__(self):
+        self.visits = 1 # Always start with 1 visit (this one)
+        self.wins = 0
+        self.draws = 0
+        self.losses = 0
+        self.meanValue = 0
+        self.ucb = 0
+
+    def updateMeanValue(self):
+        self.meanValue = (self.wins - self.losses) / (self.wins + self.draws + self.losses)
+
+    def updateUcb(self, parentVisits: int):
+        self.ucb = self.meanValue + UCB_CONSTANT * math.sqrt(math.log(parentVisits) / self.visits)
 
 # MCST Node
 class Node:
@@ -19,12 +36,88 @@ class Node:
         self.children = children
         self.stats = stats
 
-    def isNonTerminal(self):
-        return len(self.children) > 0
+    def updateStats(self, newResult):
+        if newResult == 1:
+            self.stats.wins += 1
+        elif newResult == 0:
+            self.stats.draws += 1
+        elif newResult == -1:
+            self.stats.losses += 1
+        else:
+            raise ValueError("unexpected value in result")
+        self.stats.updateMeanValue()
 
+        self.stats.updateUcb(self.parent.stats.visits)
+    
+    def isRoot(self):
+        return self.parent == None
+    
+# Specific node for Connect4 (Inheritance)
+class ConnectFour(Node):
+    def __init__(self, parent: Self, children: list[Self], stats: Stats, boardState: Board, columnDroppedIn: int, activePlayer: str):
+        super().__init__(parent, children, stats)
+        self.boardState = boardState
+        self.activePlayer = activePlayer
+        self.isFullyExpanded = False
+        # since each turn involves the player dropping a disc into a column, this node stores the column dropped into for this turn
+
+    '''
+    Do expansion by randomly picking a move that hasn't been explored yet from this state
+
+    Raises IndexError if all moves from this state have been explored
+    '''
+    def createUnexploredChild(self):
+        # Find all the moves that haven't been made yet by the children
+        unexploredColumns = list(range(game.NUM_COLUMNS))
+        for child in self.children:
+            if child.columnDroppedIn in unexploredColumns:
+                unexploredColumns.remove(child.columnDroppedIn)
+        if len(unexploredColumns) == 0:
+            self.isFullyExpanded = True
+            raise IndexError("All moves from this state have been explored")
+
+        # Choose a random column to drop the disc into
+        moveToMake = random.choice(unexploredColumns)
+
+        if self.activePlayer == 'X':
+            nextPlayer = 'O'
+        else:
+            nextPlayer = 'X'
+
+        # Create and return this child node
+        newChild = ConnectFour(
+            self,
+            [],
+            None,
+            game.makeMove(nextPlayer, moveToMake, self.boardState),
+            moveToMake,
+            nextPlayer
+        )
+        self.children.append(newChild)
+        return newChild
+    
+    '''
+    Check to see if this move brought the game into a win condition
+
+    Returns true if this move resulted in a win for the active player
+    '''
+    def isTerminal(self):
+        return game.hasWon(self.activePlayer, self.boardState)
+
+'''
+Get the result of this rollout
+
+Returns +1 for a win for the computer, 0 for a draw, and -1 for a loss for the computer.
+'''
+def result(node: ConnectFour) -> int:
+    if node.isTerminal():
+        if node.activePlayer == COMPUTER_PLAYER:
+            return 1
+        elif node.activePlayer == OPPOSING_PLAYER:
+            return -1
 
 # main function
-def monteCarloTreeSearch(root: Node):
+def monteCarloTreeSearch(root: ConnectFour):
 
     for iteration in range(MAX_SEARCH_TIME):
         leaf = traverse(root)
@@ -33,17 +126,29 @@ def monteCarloTreeSearch(root: Node):
 
     return bestChild(root)
 
-# node traversal function
-def traverse(node):
-    while fullyExpanded(node):
-        node = bestUct(node)
+# ranking function helper
+def getUcb(node):
+    return node.stats.ucb
 
-    # if this is a leaf node
-    return pickUnvisited(node.children) or node
+# ranking function
+def bestUcb(children: list[ConnectFour]):
+    children.sort(key=getUcb)
+    return children[0]
+
+# node traversal function
+def traverse(node: ConnectFour):
+    while node.isFullyExpanded:
+        node = bestUcb(node.children)
+
+    # see if this is a leaf node
+    try:
+        return node.createUnexploredChild()
+    except IndexError:
+        return node
 
 # function for the result of the simulation
 def rollout(node):
-    while node.isNonTerminal():
+    while not node.isTerminal():
         node = rolloutPolicy(node)
     return result(node)
 
@@ -52,13 +157,17 @@ def rolloutPolicy(node):
     return random.choice(node.children)
 
 # backpropogation function
-def backpropogate(node, result):
-    if isRoot(node):
+def backpropogate(node: ConnectFour, result):
+    if node.isRoot():
         return
-    node.stats = updateStats(node, result)
+    node.updateStats(result)
     backpropogate(node.parent)
 
-# select the best child node
-# with the highest number of visits
-def bestChild(node):
-    return childWithHighestNumberOfVisits
+def getNumVisits(node: ConnectFour):
+    return node.stats.visits
+
+# select the best child node with the highest number of visits
+def bestChild(node: ConnectFour) -> ConnectFour:
+    node.children.sort(key=getNumVisits)
+    return node.children[0]
+
--- experiments/tests/test_monteCarloSearchTree.py
+++ experiments/tests/test_monteCarloSearchTree.py
@@ -1,16 +1,63 @@
 import unittest
+import math
 from experiments import monteCarloSearchTree as mcst
+from experiments import connectFour as game
 
 class TestNode(unittest.TestCase):
 
     def setUp(self):
-        self.root = mcst.Node(None, [], None)
-        self.child = mcst.Node(self.root, [], None)
+        self.root = mcst.Node(None, [], mcst.Stats())
+        self.child = mcst.Node(self.root, [], mcst.Stats())
         self.root.children.append(self.child)
-        
-    def test_isNonTerminal(self):
-        self.assertTrue(self.root.isNonTerminal())
-        self.assertFalse(self.child.isNonTerminal())
+
+    def test_updateStats(self):
+        self.child.updateStats(1)
+        self.assertTrue(self.child.stats.wins == 1)
+        self.assertTrue(self.child.stats.losses == 0)
+        self.assertTrue(self.child.stats.draws == 0)
+
+    def test_isRoot(self):
+        self.assertTrue(self.root.isRoot())
+        self.assertFalse(self.child.isRoot())
+
+class TestStats(unittest.TestCase):
+
+    def setUp(self):
+        self.winningTestStats = mcst.Stats()
+        self.winningTestStats.visits = 12
+        self.winningTestStats.wins = 7
+        self.winningTestStats.losses = 3
+        self.winningTestStats.draws = 2
+        self.winningTestStats.updateMeanValue()
+        self.winningTestStats.updateUcb(4)
+
+        self.losingTestStats = mcst.Stats()
+        self.losingTestStats.visits = 17
+        self.losingTestStats.wins = 6
+        self.losingTestStats.losses = 7
+        self.losingTestStats.draws = 4
+        self.losingTestStats.updateMeanValue()
+        self.losingTestStats.updateUcb(5)
+
+    def test_updateMeanValue(self):
+        self.assertEqual(self.winningTestStats.meanValue, 1/3)
+        self.assertEqual(self.losingTestStats.meanValue, -1/17)
+
+    def test_updateUcb(self):
+        self.assertEqual(self.winningTestStats.ucb, 1/3+mcst.UCB_CONSTANT*math.sqrt(math.log(4)/12))
+        self.assertEqual(self.losingTestStats.ucb, -1/17+mcst.UCB_CONSTANT*math.sqrt(math.log(5)/17))
+
+class TestConnectFour(unittest.TestCase):
+
+    def setUp(self):
+        self.board = game.newBoard()
+        game.makeMove("O", 3, self.board)
+        self.root = mcst.ConnectFour(None, [], mcst.Stats(), self.board, 3, "O")
+        game.makeMove("X", 3, self.board)
+        self.child = mcst.Node(self.root, [], mcst.Stats(), self.board, 3, "X")
+        self.root.children.append(self.child)
+
+
 
 if __name__ == '__main__':
     unittest.main()
