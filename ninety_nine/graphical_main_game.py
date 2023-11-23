47098da90f433c54c7bfd70083e7d27c5efc24bc
Skyler Atchison <skyler.atchison145@myci.csuci.edu>
2023-11-23 14:44:01 -0800

    Renamed images

--- ninety_nine/graphical_main_game.py
+++ ninety_nine/graphical_main_game.py
@@ -1,26 +1,52 @@
 import pygame
+from ninety_nine import ninety_nine_state as game
+import random
+from ninety_nine.constants import (
+    PlayerTypes,
+    Rank,
+    Suit,
+    NUM_CARDS_IN_BID,
+    PLAYER_TYPES,
+    NUM_TRICKS,
+    NUM_PLAYERS,
+)
+
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
-pygame.init()
 
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    clock = pygame.time.Clock()
 
-clock = pygame.time.Clock()
+    while True:
+        # process player inputs
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                raise SystemExit
 
-while True:
-    # process player inputs
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            raise SystemExit
+        # logical updates here
 
-    # logical updates here
+        screen.fill("green")
 
-    screen.fill("purple")
+        # render graphics here
 
-    # render graphics here
+        pygame.display.flip()
+        clock.tick(60)
 
-    pygame.display.flip()
-    clock.tick(60)
+
+def draw_hand(hand: list, first_card_x: int, first_card_y: int):
+    for card in hand:
+        pass
+
+
+def get_image_from_card(card: game.Card):
+
+
+
+
+if __name__ == '__main__':
+    main()
